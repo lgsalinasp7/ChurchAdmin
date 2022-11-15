503b3207f5035065eb2ed0cf63fc2169e20e703d
Luis Salinas <lgsalinasp@gmail.com>
2022-11-15 11:40:00 -0500

    Primeros pasos de Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
-const routes: Routes = [];
+const approutes: Routes = [
+  {path:'dashboard', component:DashboardComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:LoginComponent},
+  {path:'progress', component:ProgressComponent},
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'},
+
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(approutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const APP_ROUTES =RouterModule.forRoot(approutes,{useHash:true})
