dc77d658e2f66f1d933eec66006f38d2ca651997
Luis Salinas <lgsalinasp@gmail.com>
2022-11-15 22:31:38 -0500

    modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,14 @@
-import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { RegisterComponent } from './login/register.component';
+import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import {PagesComponent} from './pages/pages.component'
 
 const approutes: Routes = [
-  {
-   path:'',
-    component:PagesComponent,
-    children: [
-      {path:'dashboard', component:DashboardComponent},
-      {path:'progress', component:ProgressComponent},
-      {path:'graficas1', component:Graficas1Component},
-      {path:'', redirectTo:'/dashboard', pathMatch:'full'}
-    ]},
+
    {path:'register', component:RegisterComponent},
    {path:'login', component:LoginComponent},
    {path:'**', component:NopagefoundComponent},
-
-
 ];
 
 @NgModule({
